799e01bed254e414040933ad66849a129d077615
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 18:47:44 -0300

    Add files via upload

---
+++ Exercício 6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercício_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = 0;
+            string idademaior;
+            idademaior = "";
+            string[] nome;
+            int[] idade;
+
+            int n = int.Parse(Console.ReadLine());
+
+            nome = new string[n];
+            idade = new int[n];
+
+            for(int i=0;i<n;i++)
+            {
+                string[] vetor = Console.ReadLine().Split(' ');
+                nome[i] = vetor[0];
+                idade[i] = int.Parse(vetor[1]);
+
+                if(maior==0)
+                {
+                    maior = idade[i];
+                   idademaior = nome[i];
+                }
+                else if(maior < idade[i])
+                {
+                    maior = idade[i];
+                    idademaior = nome[i];
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pessoa mais velha : " + idademaior);
+        }
+    }
+}
